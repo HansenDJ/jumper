b1d58af5fb252cd5bf2ed54a6b2605893c19566c
= <jefferysuperman@gmail.com>
2022-02-02 11:47:05 -0700

    Added Program entry point and Director

---
+++ src/Director.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+namespace Game
+{
+
+    public class Director
+    {
+        public static bool playingNow = true;
+         public void StartGame()
+        {
+           
+            while (playingNow) // Call methods and functions from here, infinite loop until PlayNow is false;
+            {
+                
+            }
+
+        }
+
+    }
+}
