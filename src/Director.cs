e33186ec39fff0354a70a5de0882387773940017
Krisst <krisstopher03@gmail.com>
2022-02-03 22:04:05 -0700

    called  mainDisplay in director class

--- src/Director.cs
+++ src/Director.cs
@@ -6,13 +6,14 @@ namespace Game
 
     public class Director
     {
+        TerminalService terminalService = new TerminalService ();
         public static bool playingNow = true;
         public void StartGame()
         {
             
             while (playingNow) // Call methods and functions from here, infinite loop until PlayNow is false;
             {
-               
+              mainDisplay();
             }
 
         }
