af4a086c99b6d0b92632767553bea9ad7ba4b341
= <jefferysuperman@gmail.com>
2022-02-03 21:07:58 -0700

    Random word chosen from a file and returned through Guess

--- src/Guess.cs
+++ src/Guess.cs
@@ -1,7 +1,15 @@
-using Game;
+using System;
 
 namespace Game;
 class Guess
-{
-    
+{   
+    public string generateWord() {
+      string line = File.ReadLines("wordList.txt").ElementAt(randomNumber());
+        return line;
+    }
+    private int randomNumber() {
+        Random random = new Random();
+
+        return random.Next(1,1000);
+    }
 }
