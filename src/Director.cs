6dbeac76f7e710b14a2561a4b420e2344abf3ae9
= <jefferysuperman@gmail.com>
2022-02-03 22:41:08 -0700

    Added the guess methods to Director

--- src/Director.cs
+++ src/Director.cs
@@ -11,18 +11,22 @@ namespace Game
         public void StartGame()
         {
             /* Example Get Word
-            Guess guess = new Guess();
+            
             string word = guess.getWord();
             Console.WriteLine(word);
             Console.WriteLine(guess.getHashedWord());
             */
-            
+            Guess guess = new Guess();
+            guess.getWord();
             while (playingNow) // Call methods and functions from here, infinite loop until PlayNow is false;
             {
+              
               terminalService.mainDisplay();
+              
             }
 
         }
+
 
     }
 }
