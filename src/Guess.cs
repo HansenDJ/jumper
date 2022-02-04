1aa81f16e8e2d056c1355f41006247a0cf162be5
Roran47 <ekcook@live.com>
2022-02-03 21:45:56 -0800

    getword added

--- src/Guess.cs
+++ src/Guess.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Collections.Generic;
 
 namespace Game;
 class Guess
 {   
-    public string generateWord() {
+    private string generateWord() {
       string line = File.ReadLines("wordList.txt").ElementAt(randomNumber());
         return line;
     }
@@ -11,5 +12,29 @@ class Guess
         Random random = new Random();
 
         return random.Next(1,1000);
+
+    }
+    
+    public string getWord(){
+        bool conditionmet = false;
+        string currentWord = "";
+        while(conditionmet == false){
+        currentWord = generateWord();
+        int length = currentWord.Length;
+        if (length >= 3)
+        {
+           conditionmet = true;
+        }
+    }
+    return currentWord;
     }
 }
+
+
+        // list of words txt words 
+        // random number created 
+        // grab the word on that line
+        // block out 3 three of the letters in the word 
+        // 
+
+    
